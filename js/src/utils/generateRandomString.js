02c7c50c21280eae194e0a304fb516f53a9651b0
marco79423 <marco79423@gmail.com>
2021-06-29 18:04:23 +0800

    新增 js 測試

--- js/src/utils/generateRandomString.js
+++ js/src/utils/generateRandomString.js
@@ -1,7 +1,9 @@
 export const DEFAULT_LENGTH = 6
 export const DEFAULT_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
-export default function generateRandomString(length = DEFAULT_LENGTH, charset = DEFAULT_CHARSET) {
+export default function generateRandomString(config) {
+  const {length = DEFAULT_LENGTH, charset = DEFAULT_CHARSET} = config || {}
+
   let result = ''
   const charsetLength = charset.length
   for (let i = 0; i < length; i++) {
