9e32b8ff13b1087e5acc96093662fbe1eaffa627
marco79423 <marco79423@gmail.com>
2021-06-29 14:15:53 +0800

    新增 js 版的 paji-sdk，並升為 0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# CHANGELOG
+
+## [Unreleased]
+
+
+## [0.2.0] - 2021-06-29
+
+* 新增 js 部分的 sdk
---
+++ js/src/utils/generateRandomString.js
@@ -0,0 +1,11 @@
+export const DEFAULT_LENGTH = 6
+export const DEFAULT_CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+export default function generateRandomString(length = DEFAULT_LENGTH, charset = DEFAULT_CHARSET) {
+  let result = ''
+  const charsetLength = charset.length
+  for (let i = 0; i < length; i++) {
+    result += charset.charAt(Math.floor(Math.random() * charsetLength))
+  }
+  return result
+}
